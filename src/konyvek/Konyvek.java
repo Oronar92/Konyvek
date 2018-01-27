bf09b6f2a7cdafb0c9552353bab15e6ca9055dff
Oronar <>
2018-01-27 10:27:26 +0100

    Első változat

---
+++ src/konyvek/Konyvek.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package konyvek;
+
+/**
+ *
+ * @author t1
+ */
+public class Konyvek {
+
+  public static void main(String[] args) {
+     DB ab=new DB(); 
+     ab.kiir(1962);
+  }
+   
+}
