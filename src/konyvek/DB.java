df6f2aa07460857b7f096b1728f56b8894db5238
Oronar <t1@192.168.56.1>
2018-01-27 11:34:02 +0100

    Új könyv felvétele

--- src/konyvek/DB.java
+++ src/konyvek/DB.java
@@ -5,7 +5,6 @@
  */
 package konyvek;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -13,36 +12,37 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
-
 /**
  *
  * @author t1
  */
 public class DB {
+
     Connection kapcs = null;
     Statement parancs = null;
     ResultSet eredmeny = null;
     PreparedStatement ekp = null;
-    
+
     final String dbUrl = "jdbc:mysql://localhost:3306/konyvek";
     final String user = "root";
     final String pass = "";
-    
+
+
     public DB() {
         try {
             // kapcsolódás
             kapcs = DriverManager.getConnection(dbUrl, user, pass);
             System.out.println("A kapcsolat létrejött.");
-            
+
             // parancs létrehzása
             parancs = kapcs.createStatement();
-            
+
         } catch (SQLException ex) {
             System.out.println("" + ex);
         }
     }
-    
+
+
     public void kiir(int ev) {
         try {
             ekp = kapcs.prepareStatement("SELECT * FROM adatok WHERE ev = ?");
@@ -57,8 +57,31 @@ public class DB {
                         eredmeny.getInt("ev"));
             }
         } catch (SQLException ex) {
-                    System.out.println("Hiba a lekérdezésben!");
-                    }
+            System.out.println("Hiba a lekérdezésben!");
         }
     }
+
+    public void uj(String szerzo, String cim, String eredeti, int ev) {
+        try {
+            String s = "INSERT INTO adatok (szezro, cim, eredeti, ev) ('"
+                    + szerzo + "', '" + cim + "','"
+                    + eredeti + "'," + ev + ");";
+            int sorok = parancs.executeUpdate(s);
+            System.out.println(sorok + " sor hozzáadva.");
+        } catch (SQLException ex) {
+            System.out.println("Nem sikerült a frissités!");
+        }
+    }
+
+    public void torol(int id) {
+        try {
+            String s = "DELETE FROM adatok WHERE id=" + id + ";";
+            int sorok = parancs.executeUpdate(s);
+            System.out.println(sorok + " sor törölve");
+            
+        }catch (SQLException ex) {
+            System.out.println("Nem sikerült a törlés!");
+        }
+    }
+}
 
