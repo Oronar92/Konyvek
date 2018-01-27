df3f79461ae40443252fcb21e6c9a62d51ff12b7
Oronar <t1@192.168.56.1>
2018-01-27 10:46:25 +0100

    Új főprogram

--- src/konyvek/Konyvek.java
+++ src/konyvek/Konyvek.java
@@ -5,6 +5,8 @@
  */
 package konyvek;
 
+import java.util.Scanner;
+
 /**
  *
  * @author t1
@@ -13,7 +15,33 @@ public class Konyvek {
 
   public static void main(String[] args) {
      DB ab=new DB(); 
-     ab.kiir(1962);
+     Scanner bill = new Scanner(System.in, "cp1250");
+     boolean tovabb = true;
+     String szerzo, cim, eredeti;
+     int ev;
+    
+     while (tovabb) {
+         System.out.println("1-listázás 2-új 3-törlés 0-kilépés");
+         System.out.print("Válasz: ");;
+         String v = bill.nextLine();
+         switch (v.charAt(0)) {
+             case '1':
+                 System.out.print("Év: ");
+                 ev = bill.nextInt();
+                 bill.nextLine();
+                 ab.kiir(ev);
+                 break;
+             case '2':
+                 
+                 break;
+             case '3':
+                 
+                 break;
+             default:
+                 tovabb = false;
+                 
+         }
+     }
   }
    
 }
